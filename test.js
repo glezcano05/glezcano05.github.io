388549c6e0f4c8cf0b48eb433b600a2ab012d3d2
glezcano05 <135974716+glezcano05@users.noreply.github.com>
2023-09-12 15:21:53 -0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,11 @@
+let num = prompt("Ingrese el número");
+let factorial = 1;
+
+while (num < 0) {
+    console.log("Error! El número ingresado debe ser mayor o igual a 0. Inténtelo nuevamente.");
+    num = prompt("Ingrese el número");
+}
+for (let i = 1; i <= num; i++) {
+    factorial *= i;
+}
+console.log("El resultado de " + num + "! es: " + factorial + ".");
